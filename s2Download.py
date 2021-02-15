20f89b637963f7e374d50d9bfcebf28459e593de
cgcandido <chriscandido93@gmail.com>
2021-02-15 13:53:09 +0800

    Update Sentinel Image Downloader

--- s2Download.py
+++ s2Download.py
@@ -1,24 +1,59 @@
 ## Import Python Modules
-import sentinelhub
-import pandas 
-import datetime
+## Import Python Modules
+import folium 
+import numpy as np
+import geopandas as gpd
+import matplotlib.pyplot as plt
 
-## Tile Spricification 
-manilaBay_tile = ["T51PTS"]
+from sentinelsat.sentinel import SentinelAPI, read_geojson, geojson_to_wkt
 
-## Start and End date 
-startDate = "2020-01-01"
-endDate = "2020-01-30"
+## Connect to Copernicus Hub using Sentinel API
+user = "chriscandido93"
+password = "nvenesyn5868"
 
-dates = pandas.date_range(start=startDate, end=endDate)
+api = SentinelAPI(user, password, 'https://scihub.copernicus.eu/dhus')
 
-for tile in manilaBay_tile:
-    print ("Downloading tile: " + tile)
-    for date in dates:
-        print (str(date.date()) + " ...")
-        try:
-            sentinelhub.download_safe_format(tile=(tile, str(date.date())), entire_product=True)
-        except Exception as ex:
-            template = "No image for the specified tile / date combination could be found"
-            message = template.format(type(ex).__name__)
-            print (message)
+## Map of Manila bay Watershed
+m = folium.Map([14.5995, 120.9842], zoom_start=8)
+boundary = r'D:\\MapABLE\\Landcover Mapping\\Shapefile\\manilaBayWatershed_boundary.geojson'
+folium.GeoJson(boundary).add_to(m)
+m
+
+## Search Imnage tile using Geojson Boundary
+footprint = geojson_to_wkt(read_geojson(boundary))
+
+products = api.query(footprint,
+                     date = ('20200101', '20200131'),
+                     platformname = 'Sentinel-2',
+                     processinglevel = 'Level-2A',
+                     cloudcoverpercentage = (0, 20))
+
+## Convert to Geodataframe
+api.to_geodataframe(products)
+products_df = api.to_dataframe(products)
+
+## Plot Areas of Image Tiles
+areas = api.to_geodataframe(products)
+ax = areas.plot(column='uuid', cmap=None, figsize=(20, 20))
+areas.apply(lambda x: ax.annotate(s=x.uuid, xy=x.geometry.centroid.coords[0], ha='center'),axis=1)
+
+gdf2 = gpd.read_file(boundary)
+f, ax = plt.subplots(1)
+areas.plot(ax=ax,column='uuid',cmap=None,)
+gdf2.plot(ax=ax)
+plt.show()
+
+## Search by TileName
+s2tiles = []
+tileName = api.to_geodataframe(products)['title']
+
+count = 0
+
+for tile in tileName:
+    if tile.split("_")[5] == "T51PUR":
+        s2tiles.append(tileName.index[count])
+    count += 1
+s2tiles
+
+## Download Sentinel Images
+api.download_all(s2tiles, directory_path='D:\Misc\S2_Images')
